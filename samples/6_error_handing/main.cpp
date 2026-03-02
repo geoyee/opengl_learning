981ca49e4cf2982e747ff897def46adb22dfc47f
geoyee <geoyee@yeah.net>
2026-03-02 21:50:24 +0800

    feat: Add error handing

---
+++ samples/6_error_handing/main.cpp
@@ -0,0 +1,170 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#define GLCALL(func)                                                                                                   \
+    {                                                                                                                  \
+        GLClearError();                                                                                                \
+        func;                                                                                                          \
+        if (!GLLogError(#func, __FILE__, __LINE__))                                                                    \
+        {                                                                                                              \
+            __debugbreak();                                                                                            \
+        }                                                                                                              \
+    }                                                                                                                  \
+    while (0)
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR)
+        ;
+}
+
+static bool GLLogError(const char *func_name, const char *file, int line)
+{
+    while (GLenum code = glGetError())
+    {
+        std::cerr << "[OpenGL ERROR] " << file << " " << func_name << " -> (" << line << "): " << code << std::endl;
+        return false;
+    }
+    return true;
+}
+
+static std::string ParseShader(const std::string& file)
+{
+    std::ifstream fs(file);
+    std::ostringstream oss;
+    if (!fs.is_open())
+    {
+        return "";
+    }
+    oss << fs.rdbuf();
+    return oss.str();
+}
+
+static unsigned int CompilerShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char *src = source.data();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    /* Error handing */
+    int res;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &res);
+    if (res == GL_FALSE)
+    {
+        int len;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+        std::vector<char> msg(len);
+        glGetShaderInfoLog(id, len, &len, msg.data());
+        std::cerr << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+                  << " shader: " << reinterpret_cast<char const *>(msg.data()) << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompilerShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompilerShader(GL_FRAGMENT_SHADER, fragmentShader);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    /* Generally not deleted, for ease of debugging and suchlike */
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
+
+int main(void)
+{
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Initialize glew (after made context) */
+    if (glewInit() != GLEW_OK)
+    {
+        return -1;
+    }
+
+    /* Display the version of OpenGL */
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    // clang-format off
+    float positions[] = {
+        -0.5f, -0.5f, // 0
+         0.5f, -0.5f, // 1
+         0.5f,  0.5f, // 2
+        -0.5f,  0.5f  // 3
+    };
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    // clang-format on
+
+    /* Create a buffer and bind data */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    /* How to parse the configuration data */
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+
+    /* Create an index buffer and bind data */
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    /* Create shader */
+    std::string shader_path = "resource/shaders/";
+    std::string vertex_shader_src = ParseShader(shader_path + "basic_vertex.glsl");
+    std::string fragment_shader_src = ParseShader(shader_path + "basic_fragment.glsl");
+    unsigned int shader = CreateShader(vertex_shader_src, fragment_shader_src);
+    glUseProgram(shader);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        GLCALL(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr)); // GL_INT -> GL_UNSIGNED_INT
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    glDeleteProgram(shader);
+    return 0;
+}
